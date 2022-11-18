48feedfd1cc9334f7c7b049bc4e7b41163204f32
nyxfor13days <nyxfor13days@gmail.com>
2022-11-18 13:20:29 +0530

    Added lualine plugin

--- neovim/.config/nvim/lua/default/plugins.lua
+++ neovim/.config/nvim/lua/default/plugins.lua
@@ -44,5 +44,11 @@ return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use "nvim-lua/plenary.nvim"
   use "nvim-lua/popup.nvim"
+
+  -- UI Plugins
   use "navarasu/onedark.nvim"
+  use { 
+    "nvim-lualine/lualine.nvim",
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
