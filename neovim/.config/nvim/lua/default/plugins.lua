a5c4a4e9381a2b512e296b54abc9fba02e49a51a
Nyx <harsh1739371@gmail.com>
2022-11-20 13:49:39 +0530

    Added Basic Completions

--- neovim/.config/nvim/lua/default/plugins.lua
+++ neovim/.config/nvim/lua/default/plugins.lua
@@ -70,4 +70,14 @@ return require("packer").startup(function(use)
   -- Telescope
   use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
   use { "nvim-telescope/telescope.nvim", branch = "0.1.x" }
+
+  -- Autocompletions
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+
+  -- Snippets
+  use "L3MON4D3/LuaSnip"
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
 end)
